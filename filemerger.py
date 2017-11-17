aea9ed88d37a064de9a87216cb899d3bcf63cf00
mayank6255 <33080454+mayank6255@users.noreply.github.com>
2017-11-17 14:56:02 +0530

    Add files via upload

---
+++ filemerger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 17 14:49:37 2017
+
+@author: mayankparitosh
+"""
+
+import sys
+# Read in the DLL
+fd = open( sys.argv[1], "rb" )
+file_contents = fd.read()
+fd.close()
+print ("[*] Filesize: %d" % len( file_contents ))
+# Now write it out to the ADS
+fd = open( "%s:%s" % ( sys.argv[2], sys.argv[1] ), "wb" )
+fd.write( file_contents )
+fd.close()
